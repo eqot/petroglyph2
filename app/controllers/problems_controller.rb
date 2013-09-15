1073d56f37579a6a14ce6dc4701003e7368e1e1b
Ikuo Terado <eqobar@gmail.com>
2013-09-15 13:45:16 +0900

    Added new problem view

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -8,4 +8,20 @@ class ProblemsController < ApplicationController
 		@problem = Problem.find(params[:id])
 	end
 
+	def new
+		@problem = Problem.new
+	end
+
+	def create
+		@problem = Problem.new(problem_params)
+		@problem.save
+		redirect_to problems_path
+	end
+
+	private
+
+	    def problem_params
+	    	params[:problem].permit(:title)
+	    end
+
 end
