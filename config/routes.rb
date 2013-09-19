8b6bbabc64e07f2e79a28a2352a1d99d43aa3327
Ikuo Terado <eqobar@gmail.com>
2013-09-19 23:01:48 +0900

    Change root to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Probless2::Application.routes.draw do
 
+  get "static_pages/home"
+  get "static_pages/about"
   resources :problems do
     resources :solutions, only: [:create, :destroy]
   end
 
-  root 'problems#index'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
