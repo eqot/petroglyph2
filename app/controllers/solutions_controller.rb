09c77cde466bb4d00bb37c10ab523c8a3038c8cc
Ikuo Terado <eqobar@gmail.com>
2013-09-19 22:57:14 +0900

    Add solution model and controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,2 +1,21 @@
 class SolutionsController < ApplicationController
+
+  def create
+    @problem = Problem.find(params[:problem_id])
+    @solution = @problem.solutions.create(solution_params)
+    redirect_to problem_path(@problem.id)
+  end
+
+  def destroy
+    @solution = Solution.find(params[:id])
+    @solution.destroy
+    redirect_to problem_path(params[:problem_id])
+  end
+
+  private
+
+  def solution_params
+    params[:solution].permit(:title, :description)
+  end
+
 end
