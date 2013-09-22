ed47632f2c66df7452655f3c8a95087a4d31bb6d
Ikuo Terado <eqobar@gmail.com>
2013-09-23 01:22:33 +0900

    Add test for links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,4 +25,25 @@ describe "Static pages" do
 
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+
+    click_link "About"
+    expect(page).to have_title("#{base_title} | About")
+    click_link "Problems"
+    expect(page).to have_title("#{base_title} | Problems")
+    click_link "Solutions"
+    expect(page).to have_title("#{base_title} | Solutions")
+    click_link "Home"
+    expect(page).to have_title("#{base_title}")
+
+    click_link "Learn more >>"
+    expect(page).to have_title("#{base_title} | About")
+    click_link "probless"
+    expect(page).to have_title("#{base_title}")
+
+    click_link "Sign up"
+    expect(page).to have_title("#{base_title} | Sign up")
+  end
+
 end
