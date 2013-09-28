fd37e735c7e48079ea8fa7a438c40c7020e8b66b
Ikuo Terado <eqobar@gmail.com>
2013-09-28 13:44:49 +0900

    Support video to be added

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,26 @@
 class VideosController < ApplicationController
 
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = current_user.videos.build(video_params)
+    if @video.save
+      redirect_to videos_path
+    else
+      render 'new'
+    end
+  end
+
   def index
     @videos = Video.paginate(page: params[:page])
   end
+
+  private
+
+    def video_params
+      params.require(:video).permit(:url, :title, :description)
+    end
 
 end
