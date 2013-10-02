caaf5b21212de4b4c6b6a8834062e37fab2959af
Ikuo Terado <eqobar@gmail.com>
2013-10-03 04:06:56 +0900

    Increment number of unicorn's worker thread

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,7 +1,7 @@
 pid "tmp/pids/unicorn.pid"
 # listen 8080
 listen "/var/tmp/.unicorn.sock"
-worker_processes 1
+worker_processes 2
 timeout 20
 stdout_path "log/unicorn-out.log"
 stderr_path "log/unicorn-err.log"
