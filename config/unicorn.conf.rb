d3af5f50bd5be367a7518eda36c4afd9d98c0d73
Ikuo Terado <eqobar@gmail.com>
2013-10-07 23:13:13 +0900

    Minor changes in unicorn configuration

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,7 +1,8 @@
-pid "tmp/pids/unicorn.pid"
+pid "/var/tmp/unicorn.pid"
 # listen 8080
 listen "/var/tmp/.unicorn.sock"
 worker_processes 2
 timeout 20
 stdout_path "log/unicorn-out.log"
 stderr_path "log/unicorn-err.log"
+preload_app true
