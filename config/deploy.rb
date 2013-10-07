12d262b2182f3994979411c8d2adef4eaf4c1877
Ikuo Terado <eqobar@gmail.com>
2013-10-08 02:32:52 +0900

    Refactor configuration file for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,14 @@
 #require "bundler/capistrano"
 
 set :application, "petroglyph"
-# set :repository,  "set your repository location here"
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 #set :domain, "dummy.com"
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
-#role :db,  domain, :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
-set :keep_releases, 5
+### Deployment
 
 set :user, "ec2-user"
 set :use_sudo, false
@@ -48,6 +28,12 @@ namespace :deploy do
     run "mv -f #{latest_release}/config/database.yml.custom #{latest_release}/config/database.yml"
   end
 end
+
+after "deploy:restart", "deploy:cleanup"
+set :keep_releases, 5
+
+
+### Unicorn
 
 set :unicorn_pid, "/var/tmp/unicorn.pid"
 
