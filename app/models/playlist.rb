15f2076c93fd0d118628328d181d8cf3512f3a68
Ikuo Terado <eqobar@gmail.com>
2013-10-14 04:01:20 +0900

    Sort playlist with descent order

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
 
+  default_scope -> { order('created_at DESC') }
+
   validates :title, presence: true
   validates :user_id, presence: true
 end
