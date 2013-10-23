f337c41b8330afbf537a9476307b3884738c0e1a
Ikuo Terado <eqobar@gmail.com>
2013-10-24 01:32:52 +0900

    Change the number of releases after deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
   # set :unicorn_pid, "/var/tmp/unicorn.pid"
