fdc230eba75527a73e86cf07419903fa90752e62
Ikuo Terado <eqobar@gmail.com>
2013-11-06 21:19:12 +0100

    Update db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131020025708) do
+ActiveRecord::Schema.define(version: 20131102034647) do
 
   create_table "contains", force: true do |t|
     t.integer  "video_id"
@@ -35,17 +35,24 @@ ActiveRecord::Schema.define(version: 20131020025708) do
   add_index "playlists", ["user_id", "created_at"], name: "index_playlists_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                  default: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "video_likes", force: true do |t|
     t.integer  "user_id"
